e4a3a1ffa66bf64a05c88130911dddd9029bfbb1
Tahir Yasin <tahiryasin123@gmail.com>
2022-11-04 14:57:54 +0400

    User regisration welcome email notification

---
+++ app/Listeners/SendWelcomeEmail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Notifications\SendWelcomeEmailNotification;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendWelcomeEmail
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $event->user->notify(new SendWelcomeEmailNotification($event->user));
+    }
+}
